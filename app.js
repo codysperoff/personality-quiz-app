b89ffc1c0d4dad60d9114337568ad565bb57c9dd
Cody Speroff <cnsperoff@gmail.com>
2017-08-09 17:05:43 -0500

    added variables for trait scores, added js for results

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var questionsArray = [
         correctDetails: 'When an offending player pushes, trips, or checks an opposing player violently into the boards of the hockey rink. While a player\'s intent may not be malicious, an example must be set.'
     },
 
-//QUESTION 4
+//Emotional Stability
     {
         questionText: 'I see myself as anxious and easily upset.',
         questionChoices: ['disagree strongly', 'disagree moderately', 'disagree a little', 'neither agree nor disagree', 'agree a little', 'agree moderately', 'agree strongly'],
@@ -33,7 +33,7 @@ var questionsArray = [
         correctDetails: 'There are 12 players on the ice at one time: 1 center, 2 forwards, 2 defensemen, and 1 goalie for each team.'
     },
 
-//QUESTION 5
+//Openness to new xperiences
     {
         questionText: 'I see myself being open to new experiences',
         questionChoices: ['disagree strongly', 'disagree moderately', 'disagree a little', 'neither agree nor disagree', 'agree a little', 'agree moderately', 'agree strongly'],
@@ -65,7 +65,7 @@ var questionsArray = [
         correctDetails: 'There are 30 teams in the NHL.'
     },
 
-//QUESTION 9
+//Emotional Stability Opposite
     {
         questionText: 'I see myself as calm and emotionally stable.',
         questionChoices: ['disagree strongly', 'disagree moderately', 'disagree a little', 'neither agree nor disagree', 'agree a little', 'agree moderately', 'agree strongly'],
@@ -73,7 +73,7 @@ var questionsArray = [
         correctDetails: 'There are 30 teams in the NHL.'
     },
 
-//QUESTION 10
+//Openness to new xperiences Opposite
     {
         questionText: 'I see myself as conventional and uncreative.',
         questionChoices: ['disagree strongly', 'disagree moderately', 'disagree a little', 'neither agree nor disagree', 'agree a little', 'agree moderately', 'agree strongly'],
@@ -84,8 +84,43 @@ var questionsArray = [
 ];
 
 var currentQuestionNumber = 0;
+
+//How do I give each of the options a certain score?
+
+var disStrongly = questionsArray.questionChoices[0];
+var disStrongly = 1;
+
+var disModerately = questionsArray.questionChoices[1];
+var disModerately = 2;
+
+var disLittle = questionsArray.questionChoices[2];
+var disLittle = 3;
+
+var neutralChoice = questionsArray.questionChoices[3];
+var neutralChoice = 4;
+
+var agreeLittle = questionsArray.questionChoices[4];
+var agreeLittle = 5;
+
+var agreeModerately = questionsArray.questionChoices[5];
+var agreeModerately = 6;
+
+var agreeStrongly = questionsArray.questionChoices[6];
+var agreeStrongly = 7;
+
+
+
+
 var totalNumberOfQuestion = questionsArray.length;
-var totalNumberOfCorrectAnswers = 0;
+
+var extravertScore = 0;
+var agreeablenessScore = 0;
+var conscientiousnessScore = 0;
+var emotionalStabilityScore = 0;
+var opennessScore = 0;
+
+
+//var totalNumberOfCorrectAnswers = 0;
 
 
 /*--- Step 2 - Defining functions ---*/
@@ -139,18 +174,56 @@ $(document).ready(function () {
 
         //get the user's choice
         var userSelection = $("input[class='option']:checked").val();
-        //get the correct answer choice from the question array
-        var correctAnswer = questionsArray[currentQuestionNumber].questionCorrectChoice;
-        //compare the user choice with correct answer
-        if (userSelection == correctAnswer) {
-            totalNumberOfCorrectAnswers++;
+
+        //if its the first question, add the score to extravertScore
+        //if its the second question, add the score to agreeablenessScore
+        //if its the third question, add the score to conscientiousnessScore
+        //if its the fourth question, add the score to emotionalStabilityScore
+        //if its the fifth question, add the score to opennessScore
+        //if its the sixth question, add the score to extravertScore
+        //if its the seventh question, add the score to agreeablenessScore
+        //if its the eighth question, add the score to conscientiousnessScore
+        //if its the ninth question, add the score to emotionalStabilityScore
+        //if its the tenth question, add the score to opennessScore
+
+        //if it is question 1-5, score the user's response as a normal score
+        //if it is question 6-10, score the user's response as a reversed score
+
+
+        if (currentQuestionNumber == 0) {
+
         }
-        //after the user selects an answer, diplay the question and correct answer details
-        $('#result_msg').append("<h3>Q: " + questionsArray[currentQuestionNumber].questionText + "</h3>");
-        $('#result_msg').append("<h4>A: " + questionsArray[currentQuestionNumber].correctDetails + "</h4>");
+
+
+        //get the correct answer choice from the question array
+        //        var correctAnswer = questionsArray[currentQuestionNumber].questionCorrectChoice;
+
+
+        //        //compare the user choice with correct answer
+        //        if (userSelection == correctAnswer) {
+        //            totalNumberOfCorrectAnswers++;
+        //        }
+        //        //after the user selects an answer, diplay the question and correct answer details
+        //        $('#result_msg').append("<h3>Q: " + questionsArray[currentQuestionNumber].questionText + "</h3>");
+        //        $('#result_msg').append("<h4>A: " + questionsArray[currentQuestionNumber].correctDetails + "</h4>");
 
         //if the quiz is finished, show the result-section
         if ((currentQuestionNumber + 1) == totalNumberOfQuestion) {
+            //calculate the scores
+            extravertScore = extravertScore / 2;
+            agreeablenessScore = agreeablenessScore / 2;
+            conscientiousnessScore = conscientiousnessScore / 2;
+            emotionalStabilityScore = emotionalStabilityScore / 2;
+            opennessScore = opennessScore / 2;
+
+            //display scores and links to wikipedia sites.
+
+            $('#result_msg').append("Extraversion: " + extravertScore + ". More information about extraversion can be found <a href='https://en.wikipedia.org/wiki/Big_Five_personality_traits#Extraversion'>here</a>.");
+            $('#result_msg').append("Agreeableness: " + agreeablenessScore + ". More information about agreeableness can be found <a href='https://en.wikipedia.org/wiki/Big_Five_personality_traits#Agreeableness'>here</a>.");
+            $('#result_msg').append("Conscientiousness: " + conscientiousnessScore + ". More information about conscientiousness can be found <a href='https://en.wikipedia.org/wiki/Big_Five_personality_traits#Conscientiousness'>here</a>.");
+            $('#result_msg').append("Emotional Stabiliy: " + emotionalStabilityScore + ". More information about emotional stability can be found <a href='https://en.wikipedia.org/wiki/Big_Five_personality_traits#Neuroticism'>here</a>.");
+            $('#result_msg').append("Openness to experience: " + opennessScore + ". More information about openness can be found <a href='https://en.wikipedia.org/wiki/Big_Five_personality_traits#Openness_to_experience'>here</a>.");
+
             //edit the results section to include the final score
             $('#finalScore').text(totalNumberOfCorrectAnswers + "/" + totalNumberOfQuestion);
 
